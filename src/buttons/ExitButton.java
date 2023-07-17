23e01f5f3a50670ecbdd11304cd49fb46ef338fe
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-07-17 03:22:31 +0300

    added new TODO

--- src/buttons/ExitButton.java
+++ src/buttons/ExitButton.java
@@ -1,7 +1,32 @@
 package buttons;
 
-import javax.swing.*;
+import data.TrainingDay;
+import org.codehaus.jackson.map.ObjectMapper;
 
-public class ExitButton extends JButton {
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ExitButton extends JButton {  //TODO: Реализовать завершение программы и копирование данных в json-файл
+
+    public ExitButton(String text){
+        super(text);
+    }
+
+    public void exitActionListener(HashSet<TrainingDay> trDays, String path){
+        super.addActionListener(e -> {
+            ObjectMapper mapper = new ObjectMapper(); //TODO: Реализовать запись данных в файл после завершения программы
+            try {
+                mapper.writeValue(new File(path), trDays);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            System.exit(0);
+        });
+
+
+    }
 
 }
