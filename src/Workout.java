0aaaac32828aa23f40e49674e0a52dcb3be820df
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-07-17 03:25:50 +0300

    minor changes

--- src/Workout.java
+++ src/Workout.java
@@ -11,24 +11,29 @@ import java.io.IOException;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.*;
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class Workout extends JFrame {
     public HashSet<String> days = new HashSet<>();
-    public HashMap<String, TrainingDay> trDays = new HashMap<>();
+    public HashSet<TrainingDay> trDays = new HashSet<>();
     public Workout() {
         super("Workout");
 
 
         String path = "src/data/dataAboutWorkout.json";
         File file = new File(path);
-        if (!(file.length() == 0L)) { //TODO: Реализовать считывание из json-файла в мапу
+        if (!(file.length() == 0L)) { //TODO: Реализовать считывание из json-файла в мапу если файл не пустой
+
 
         }
 
 
-
+        //Заменяем мапу на сэт
+        //Считать json в массив объектов (?)
 
 
 
@@ -44,13 +49,13 @@ public class Workout extends JFrame {
 
         this.add(p1, BorderLayout.WEST);
         this.add(p2,BorderLayout.EAST);
-        p1.setLayout(new GridLayout(3, 1));
+        p1.setLayout(new GridLayout(4, 1));
 
 
 
 
         DayButton day = new DayButton("Day"); //TODO: Настроить отображение таблицы результатов
-        day.addActionListener(days, p2);
+        day.dayActionListener(days, p2);
 
         AddButton add = new AddButton("Add"); //TODO: Настроить добавление результатов в таблицу
         add.addActionListener(days, trDays,this);
@@ -58,18 +63,19 @@ public class Workout extends JFrame {
         ResButton res = new ResButton("Results");
         res.addActionListener(p2);
 
+        ExitButton exit = new ExitButton("Exit");
+        exit.exitActionListener(trDays, path);
+
         p1.add(day);
         p1.add(add);
         p1.add(res);
+        p1.add(exit);
 
 
+    }
 
-        ObjectMapper mapper = new ObjectMapper(); //TODO: Реализовать запись данных в файл после завершения программы
-        try {
-            mapper.writeValue(new File(path), trDays);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    public void jsonToPojo(File f, HashSet<TrainingDay> trDays) throws IOException{ //TODO: ПРОТЕСТИРОВАТЬ!!!
+        ObjectMapper objectMapper = new ObjectMapper();
+        trDays.add(objectMapper.readValue(f, TrainingDay.class));
     }
 }
