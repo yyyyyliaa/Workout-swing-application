e2a0cb5d5bb0d480370463ac4f0bcc5499249026
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-07-19 00:11:50 +0300

    added table creation

--- src/buttons/DayButton.java
+++ src/buttons/DayButton.java
@@ -1,45 +1,55 @@
 package buttons;
 
+import data.TextFieldWindow;
 import data.TrainingDay;
+import table.ResultsTable;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Vector;
 
 public class DayButton extends JButton { //TODO: При выборе дня открывается пустая таблица, внизу кнопка "добавить рез-ты"
-//    Object[] columnsHeader = new String[] {"Exercise", "Number of sets/reps", "Weight, kg"};
-//    Object[][] array = new String[][] {{"Exercise", "Number of sets/reps", "Weight, kg"}, {"ex1", "3/10", "60"}, {"ex2", "3/5", "65"}, {"ex3", "4/10", "70"}};
+    private String[] workoutType = {"Deadlift", "Squat", "Bench press"};
+//    private Object[] deadliftExercises = new String[] {"Hyperextension", "Pull-ups", "Block thrust", "Biceps"};
+//    private Object[] squatExercises = new String[] {"Hyperextension", "Quadriceps", "Foot press", "Сalf muscle"};
+//    private Object[] benchPressExercises = new String[] {};
+
     public DayButton(String text) {
         super(text);
     }
 
-    public void dayActionListener(HashSet<String> days, JPanel p2) {
+    public void dayActionListener(HashSet<String> days, HashSet<TrainingDay> trDays, JPanel p2) {
         super.addActionListener(e -> {
+
             Object[] optionsToChoose = days.toArray();
+
             String getDay = (String) JOptionPane.showInputDialog(
-                    null,
+                    p2,
                     "Chose the day",
                     "Day",
                     JOptionPane.QUESTION_MESSAGE,
                     null,
                     optionsToChoose,
                     " ");
+
             if(!(getDay ==null)){
                 if(!getDay.equals(" ")){
                     this.setLabel(getDay);
-                    TrainingDay trDay = new TrainingDay(getDay);
-                    /*TODO: Здесь создается таблица, конструктор которой принимает объект TrainingDay.
-                       Так же нужно реализовать возможность добавления данных в таблицу (сделать кнопку "добавить результаты")
-                       В каком классе реализовать кнопки?????
-                       Если в ResultsTable, добавить в конструктор параметр JPanel*/
 
 
-                    //ПРО ДОБАВЛЕНИЕ РЕЗУЛЬТАТОВ: 1) Считать данные в Object[][] res, передать его в конструктор TrainingDay
-                    // 2) Сделать добавление результатов при добавлении дня???
+                    for(TrainingDay trd : trDays){
+                        if (trd.getDayMonth().equals(getDay)){
+                            p2.removeAll();
+                            ResultsTable resultsTable = new ResultsTable(trd);
+                            p2.add(resultsTable);
+                            break;
+                        }
+                    }
 
-//                    JTable table = new JTable(array, columnsHeader);
-//                    p2.add(table);
                 } else {
                     this.setLabel("Day");
                 }
