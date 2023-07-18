dcf9197ffdda127039c9c7bd22aad6da3e9049ba
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-07-19 00:08:40 +0300

    added TextField

--- src/buttons/AddButton.java
+++ src/buttons/AddButton.java
@@ -1,12 +1,21 @@
 package buttons;
 
+import data.TextFieldWindow;
 import data.TrainingDay;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class AddButton extends JButton {
+
+    private String[] workoutType = {"Deadlift", "Squat", "Bench press"};
+
+    private Object[] deadliftExercises = new String[] {"Deadlift", "Hyperextension", "Pull-ups", "Block thrust", "Biceps"};
+    private Object[] squatExercises = new String[] {"Squat", "Hyperextension", "Quadriceps", "Foot press", "Сalf muscle"};
+    private Object[] benchPressExercises = new String[] {"Bench press"};
+
 
     public AddButton(String text){
         super(text);
@@ -18,10 +27,10 @@ public class AddButton extends JButton {
                 ((m == 4 || m == 6 || m == 9 || m == 11) && d < 32 && d > 0);
     }
 
-    public void addActionListener(HashSet<String> days, HashSet<TrainingDay> trDays, JFrame w){
+    public void addActionListener(HashSet<String> days, HashSet<TrainingDay> trDays, JPanel p2, JFrame f){
         super.addActionListener(e -> {
             String data;
-            data = JOptionPane.showInputDialog ("Enter the date in the format: dd/mm");
+            data = JOptionPane.showInputDialog(p2,"Enter the date in the format: dd/mm");
             int d, m;
 
             if(data.charAt(2) == '/'){
@@ -29,18 +38,44 @@ public class AddButton extends JButton {
                 d = Integer.parseInt(tokens[0]);
                 m = Integer.parseInt(tokens[1]);
             } else {
-                JOptionPane.showMessageDialog(w,"Wrong data");
+                JOptionPane.showMessageDialog(p2,"Wrong data");
                 return;
             }
 
             if(checkDataFormat(d, m)){
                 days.add(data);
-                trDays.add(new TrainingDay(d, m));
-                /*TODO: Открывается окно с выбором типа тренировки, затем окно для добавления результатов
-                *  ПРОВЕРКА НА КОРРЕКТНОСТЬ ВВОДА ДАННЫХ!!!!!*/
 
+                //Выводим окошко где можно выбрать тип тренировки
+                String getWorkoutType = (String) JOptionPane.showInputDialog(
+                        p2,
+                        "Choose the type of workout:",
+                        "Workout type",
+                        JOptionPane.QUESTION_MESSAGE,
+                        null,
+                        workoutType,
+                        " ");
+
+
+                Object[] exercise = new Object[5];
+
+                //исходя из вида тренировки выбирается массив с упражнениями
+                if(getWorkoutType.equals("Deadlift")) exercise  = deadliftExercises;
+                else if(getWorkoutType.equals("Squat")) exercise  = squatExercises;
+                else exercise  = benchPressExercises;
+
+                String[] res = new String[5];
+
+                TextFieldWindow textFieldWindow = new TextFieldWindow(res, exercise, f);
+                textFieldWindow.setSize(new Dimension(1200,400));
+                textFieldWindow.setLayout(new GridLayout(4, 1));
+                textFieldWindow.setLocation(450,200);
+
+                textFieldWindow.setVisible(true);
+
+                TrainingDay trd = new TrainingDay(data, res, getWorkoutType, exercise);
+                trDays.add(trd);
             } else {
-                JOptionPane.showMessageDialog(w,"Wrong data");
+                JOptionPane.showMessageDialog(p2,"Wrong data");
             }
         });
 
