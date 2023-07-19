45ef56449ec849c99f6af1146b82fbcc2b92e496
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-07-20 00:59:44 +0300

    removed extra fields

--- src/view/chart/Chart.java
+++ src/view/chart/Chart.java
@@ -4,17 +4,13 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Chart extends JDialog {
-    public final int[] xFor3days = {160, 360, 560};
-    public final int[] xFor7Days = {160, 260, 360, 460, 560, 660, 760};
-    public final int[] xFor30Days = {160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400, 420, 440, 460, 480, 500, 520, 540, 560, 580, 600, 620, 640, 660, 680, 700, 720, 740};
-    public  int[] y;
-    public int daysCount;
-
     public Chart (int[] y, int daysCount, JFrame p, String text, String[] days) {
         super(p, text, true);
-        this.y = y;
-        this.daysCount = daysCount;
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        int[] xFor3days = {160, 360, 560};
+        int[] xFor7Days = {160, 260, 360, 460, 560, 660, 760};
+        int[] xFor30Days = {160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400, 420, 440, 460, 480, 500, 520, 540, 560, 580, 600, 620, 640, 660, 680, 700, 720, 740};
 
         if (daysCount==3) add(new DrawingComponent(xFor3days, y, daysCount, days), BorderLayout.CENTER);
         else if (daysCount==7) add(new DrawingComponent(xFor7Days, y, daysCount, days), BorderLayout.CENTER);
