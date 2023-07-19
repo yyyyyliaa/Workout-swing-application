536d353d8dcfcaeda7a6ce469fd7493f568d76ca
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-07-20 02:05:52 +0300

    added checkCorrectData

--- src/view/TextFieldWindow.java
+++ src/view/TextFieldWindow.java
@@ -4,9 +4,11 @@ import javax.swing.*;
 
 
 public class TextFieldWindow extends JDialog {
-    JTextField ex1, ex2, ex3, ex4, ex5;
+    private JFrame f;
+    private final JTextField ex1, ex2, ex3, ex4, ex5;
     public TextFieldWindow(String[] res, Object[] exercise, JFrame f){
-        super(f, "Enter results of that day in next format: Number of sets/reps, weight", true);
+        super(f, "Enter results of that day in next format:'sets'/'reps' 'weight'", true);
+        this.f = f;
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         ex1 = new JTextField(exercise[0].toString(), 25);
@@ -18,12 +20,19 @@ public class TextFieldWindow extends JDialog {
 
         JButton set = new JButton("Set results");
         set.addActionListener(e -> {
-            res[0] = ex1.getText();
-            res[1] = ex2.getText();
-            res[2] = ex3.getText();
-            res[3] = ex4.getText();
-            res[4] = ex5.getText();
-            dispose();
+            if(checkCorrectData(ex1.getText())&&checkCorrectData(ex2.getText())&&checkCorrectData(ex3.getText())&&
+                    checkCorrectData(ex4.getText())&&checkCorrectData(ex5.getText())){
+                res[0] = ex1.getText();
+                res[1] = ex2.getText();
+                res[2] = ex3.getText();
+                res[3] = ex4.getText();
+                res[4] = ex5.getText();
+                dispose();
+            } else{
+                JOptionPane.showMessageDialog(f,"Wrong data");
+                dispose();
+            }
+
         });
 
         add(ex1);
@@ -33,5 +42,23 @@ public class TextFieldWindow extends JDialog {
         add(ex5);
         add(set);
 
+    }
+
+    private boolean checkCorrectData(String str){
+        if((str.charAt(2)=='/'&&str.charAt(5)==' ')||(str.charAt(1)=='/'&&str.charAt(4)==' ')||
+                (str.charAt(2)=='/'&&str.charAt(4)==' ')||(str.charAt(1)=='/'&&str.charAt(3)==' ')){
+                    String[] tokens = str.split(" ");
+                    if(isNumber(tokens[1])){
+                        String[] tokens2 = tokens[0].split("/");
+                        return isNumber(tokens2[0]) && isNumber(tokens2[1]);
+                    } else return false;
+        } else return false;
+    }
+
+    private boolean isNumber(String str) {
+        if (str == null || str.isEmpty()) return false;
+        for (int i = 0; i < str.length(); i++)
+            if (!Character.isDigit(str.charAt(i))) return false;
+        return true;
     }
 }
