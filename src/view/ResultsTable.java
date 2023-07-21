55f212efd77714b033281eca52deaca222cc79c2
yyyyyliaMacbook <ylia.kokareva@yandex.ru>
2023-07-21 16:52:30 +0300

    minor changes

--- src/view/ResultsTable.java
+++ src/view/ResultsTable.java
@@ -3,6 +3,7 @@ package view;
 import model.TrainingDay;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Vector;
 
@@ -10,6 +11,7 @@ public class ResultsTable extends JTable {
 
     public ResultsTable(TrainingDay trainingDay){
         super(trainingDay.getData(), new Vector<>(Arrays.asList("Exercise", "Number of sets/reps", "Weight, kg")));
+        setPreferredSize(new Dimension(1200, 200));
     }
 
 }
